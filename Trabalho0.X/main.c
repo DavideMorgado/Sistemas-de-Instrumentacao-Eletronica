94ee44e309d4b3307aaba63431ee38cc2a1f71ed
Davide Morgado <davidemorgado@ua.pt>
2020-03-09 19:29:16 +0000

    Add PWM function

--- Trabalho0.X/main.c
+++ Trabalho0.X/main.c
@@ -15,8 +15,9 @@
 
 #define Prescaler           7                             
 #define Prescaler_val       256    // biggest value is chosen to obtain smaller frequencies   
-#define Freq_Base           5
-
+#define timer_freq          200
+#define TPS_256 7                   // TCKPS code for 256 pre-scaler
+#define resolution          256    
 void init(void)
 {
 	/* Configure RA3 and RC1 (led 4 and led 5) as Output */
@@ -26,6 +27,10 @@ void init(void)
 	/*define initial values for RA3 and RC1*/
     PORTAbits.RA3 = 0;
     PORTCbits.RC1 = 1;
+    
+    // Set RD0 as digital output - PWM
+    TRISDbits.TRISD0 = 0;
+    PORTDbits.RD0 = 1;
 }
 
 
@@ -35,56 +40,73 @@ void initTimer(int freq){
         IPC2bits.T2IP=5;     //set interrupt priority (1..7) *** Make sure it matches iplx in isr declaration ***
         IEC0bits.T2IE = 0;   // Disable T2 interrupts -> 0 corresponds polling
         // Timer period configuration
-        T2CONbits.TCKPS = 7; //Divide by 256 pre-scaler - Timer 2 contains this prescaler: 1,2,4,8,16,36,64,256 (correspond to the number 7)
+        T2CONbits.TCKPS = TPS_256; //Select pre-scaler
+                                   //Divide by 256 pre-scaler - Timer 2 contains this prescaler: 1,2,4,8,16,36,64,256 (correspond to the number 7)
         T2CONbits.T32 = 0;   // 16 bit timer operation
         TMR2 = 0;
         T2CONbits.TCKPS = Prescaler;    
         PR2 = PBCLOCK/(Prescaler_val*freq) - 1;      // defines timer frequency equal to Timer2_freq                                  
-        T2CONbits.TON = 1;      
-
         T2CONbits.TON=1;     // Start the timer
 }
 
 void initADC(){
-    // Initialize ADC module
-    // Polling mode, AN0 as input
-    // Generic part
-    AD1CON1bits.SSRC = 7; // Internal counter ends sampling and starts conversion
-    AD1CON1bits.CLRASAM = 1; //Stop conversion when 1st A/D converter interrupt is generated and clears ASAM bit automatically
-    AD1CON1bits.FORM = 0; // Integer 16 bit output format
-    AD1CON2bits.VCFG = 0; // VR+=AVdd; VR-=AVss
-    AD1CON2bits.SMPI = 0; // Number (+1) of consecutive conversions, stored in ADC1BUF0...ADCBUF{SMPI}
-    AD1CON3bits.ADRC = 1; // ADC uses internal RC clock
-    AD1CON3bits.SAMC = 16; // Sample time is 16TAD ( TAD = 100ns)
-    // Set AN0 as input
-    AD1CHSbits.CH0SA = 0; // Select AN0 as input for A/D converter
-    TRISBbits.TRISB0 = 1; // Set AN0 to input mode
-    AD1PCFGbits.PCFG0 = 0; // Set AN0 to analog mode
-    // Enable module
-    AD1CON1bits.ON = 1; // Enable A/D module (This must be the ***last instruction of configuration phase***)
-}
-
-int main(int argc, char** argv) {
-    init();
-    initTimer(Freq_Base);
-       // Variable declarations;
-    float res; // Sampled volatge
-
     // Init UART and redirect tdin/stdot/stderr to UART
     if(UartInit(PBCLOCK, 115200) != UART_SUCCESS) {
         PORTAbits.RA3 = 1; // If Led active error initializing UART
         while(1);
     }
-    __XC_UART = 1; /* Redirect stdin/stdout/stderr to UART1*/
-    
+    __XC_UART = 1; /* Redirect stdin/stdout/stderr to UART1*/ 
     // Disable JTAG interface as it uses a few ADC ports
     DDPCONbits.JTAGEN = 0;
     
-    initADC();
+    // Initialize ADC module
+    // Polling mode, AN0 as input
+    // Generic part
+    AD1CON1bits.SSRC = 7;        // Internal counter ends sampling and starts conversion
+    AD1CON1bits.CLRASAM = 1;     //Stop conversion when 1st A/D converter interrupt is generated and clears ASAM bit automatically
+    AD1CON1bits.FORM = 0;        // Integer 16 bit output format
+    AD1CON2bits.VCFG = 0;        // VR+=AVdd; VR-=AVss
+    AD1CON2bits.SMPI = 0;        // Number (+1) of consecutive conversions, stored in ADC1BUF0...ADCBUF{SMPI}
+    AD1CON3bits.ADRC = 1;        // ADC uses internal RC clock
+    AD1CON3bits.SAMC = 16;       // Sample time is 16TAD ( TAD = 100ns)
+    // Set AN0 as input
+    AD1CHSbits.CH0SA = 0;        // Select AN0 as input for A/D converter
+    TRISBbits.TRISB0 = 1;        // Set AN0 to input mode
+    AD1PCFGbits.PCFG0 = 0;       // Set AN0 to analog mode
+    // Enable module
+    AD1CON1bits.ON = 1;          // Enable A/D module (This must be the ***last instruction of configuration phase***)
+}
+
+void init_OC (void){
+   // Set OC1 - chipKIT Pin 3 
+    OC1CONbits.OCM = 6;          // OCM = 0b110 : OC1 in PWM mode,  (Output compare mode selected bits)
+    OC1CONbits.OCTSEL = 0;       // Timer 2 is clock source of OCM
+    OC1RS=0;                     // Compute OC1xRS value
+    OC1CONbits.ON=1;             // Enable OC1
+
+    // Start PWM generation
+    T2CONbits.TON=1; // Start the timer
     
+}
+void update_duty(int duty_cycle){
+    int fin = timer_freq*10^resolution;         //guião 7 - ac2: Resolução = log2(TPWM / TIN) = log2(fIN / fOUT)
+    //OC1RS = fin / timer_freq;  // Update DC
+    OC1RS = ((PR2 + 1) * duty_cycle)/100;
+    //resolution = log2(timer_freq);
+}
+
+int main(int argc, char** argv) {
+    init();
+    initTimer(timer_freq);
+    
+    // Variable declarations;
+    float res;                      // Sampled volatge  - ADC
+    int intensity = 0, i=0;         // - PWM
+  
+    initADC();
+    init_OC();
    // Welcome message
     printf("Prints voltage at AN0 (Pin 54 of ChipKIT)\n\n\r");
-
 
     while(1){
       // Get one sample
@@ -94,11 +116,12 @@ int main(int argc, char** argv) {
             
         // Convert to 0..3.3V 
         res = (ADC1BUF0 * 3.3) / 1023;
-
         // Output result
-        printf("Voltage: \n\r",res););
-        //printf("Temp:%f",(res-2.7315)/.01); // For a LM335 directly connected
-       
+        printf("Voltage: %f \n\r",res);
+        //printf("Temp:%f \n",(res-2.7315)/.01); // For a LM335 directly connected
+        
+        update_duty(res*10);
+        
     }
     return (EXIT_SUCCESS);
 }
