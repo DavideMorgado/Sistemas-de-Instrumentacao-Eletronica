8279682c3a132874538feda4b9bbba0d2fd88fdd
Davide Morgado <davidemorgado@ua.pt>
2020-03-09 15:57:01 +0000

    Debugging ADC code

--- Trabalho0.X/main.c
+++ Trabalho0.X/main.c
@@ -2,20 +2,20 @@
  *  Trabalho 0   
  */
 
-#include "config_bits.h"
 
 #include <xc.h>
 #include <sys/attribs.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "uart.h" 
+#include "config_bits.h"
 
 #define SYSCLK  80000000L // System clock frequency, in Hz
 #define PBCLOCK 40000000L // Peripheral Bus Clock frequency, in Hz
 
 #define Prescaler           7                             
 #define Prescaler_val       256    // biggest value is chosen to obtain smaller frequencies   
-#define Freq_Base           10
+#define Freq_Base           5
 
 void init(void)
 {
@@ -26,11 +26,10 @@ void init(void)
 	/*define initial values for RA3 and RC1*/
     PORTAbits.RA3 = 0;
     PORTCbits.RC1 = 1;
-
 }
 
-void initTimer(int Timer, int freq){
-    if (Timer == 2){ // Set timer
+
+void initTimer(int freq){
         T2CONbits.ON = 0;    // Stop timer
         IFS0bits.T2IF=0;     // Reset interrupt flag
         IPC2bits.T2IP=5;     //set interrupt priority (1..7) *** Make sure it matches iplx in isr declaration ***
@@ -44,27 +43,9 @@ void initTimer(int Timer, int freq){
         T2CONbits.TON = 1;      
 
         T2CONbits.TON=1;     // Start the timer
-    }else if(Timer == 3){
-            // Set timer
-        T3CONbits.ON = 0;    // Stop timer
-        IFS0bits.T3IF=0;     // Reset interrupt flag
-        IPC3bits.T3IP=5;     //set interrupt priority (1..7) *** Make sure it matches iplx in isr declaration ***
-        IEC0bits.T3IE = 0;   // Disable T2 interrupts -> 0 corresponds polling
-        // Timer period configuration
-        T3CONbits.TCKPS = 7; //Divide by 256 pre-scaler - Timer 2 contains this prescaler: 1,2,4,8,16,36,64,256 (correspond to the number 7)
-        //T3CONbits.T32 = 0;   // 16 bit timer operation
-        TMR3 = 0;
-        T3CONbits.TCKPS = Prescaler;    
-        PR3 = PBCLOCK/(Prescaler_val*freq) - 1;      // defines timer frequency equal to Timer2_freq                                  
-        T3CONbits.TON = 1;      
-
-        T2CONbits.TON=1;     // Start the timer
-    
-    }
 }
 
 void initADC(){
-    
     // Initialize ADC module
     // Polling mode, AN0 as input
     // Generic part
@@ -81,19 +62,19 @@ void initADC(){
     AD1PCFGbits.PCFG0 = 0; // Set AN0 to analog mode
     // Enable module
     AD1CON1bits.ON = 1; // Enable A/D module (This must be the ***last instruction of configuration phase***)
-
 }
 
-/* 
- */
 int main(int argc, char** argv) {
     init();
-    // Variable declarations;
+    initTimer(Freq_Base);
+       // Variable declarations;
     float res; // Sampled volatge
-    //initTimer(2, Freq_Base);
-    
-       // Init UART and redirect tdin/stdot/stderr to UART
-  
+
+    // Init UART and redirect tdin/stdot/stderr to UART
+    if(UartInit(PBCLOCK, 115200) != UART_SUCCESS) {
+        PORTAbits.RA3 = 1; // If Led active error initializing UART
+        while(1);
+    }
     __XC_UART = 1; /* Redirect stdin/stdout/stderr to UART1*/
     
     // Disable JTAG interface as it uses a few ADC ports
@@ -101,20 +82,21 @@ int main(int argc, char** argv) {
     
     initADC();
     
-        // Welcome message
-    printf("Prints voltage at AN0 (Pin 54 of ChipKIT)\n\r");
+   // Welcome message
+    printf("Prints voltage at AN0 (Pin 54 of ChipKIT)\n\n\r");
+
 
     while(1){
       // Get one sample
-        IFS1bits.AD1IF = 0; // Reset interrupt flag
-        AD1CON1bits.ASAM = 1; // Start conversion
+        IFS1bits.AD1IF = 0;          // Reset interrupt flag
+        AD1CON1bits.ASAM = 1;        // Start conversion
         while (IFS1bits.AD1IF == 0); // Wait fo EOC
-
+            
         // Convert to 0..3.3V 
         res = (ADC1BUF0 * 3.3) / 1023;
 
         // Output result
-        printf("Voltage: %f\n\r",res);
+        printf("Voltage: \n\r",res););
         //printf("Temp:%f",(res-2.7315)/.01); // For a LM335 directly connected
        
     }
