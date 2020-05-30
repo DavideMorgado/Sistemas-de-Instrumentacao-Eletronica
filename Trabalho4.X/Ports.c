e2d8df3604b97fac2f3f5f3387ffc9b72d92742c
Davide Morgado <davidemorgado@ua.pt>
2020-05-30 14:22:35 +0100

    Add ports for PWM - OC2 and OC3

--- Trabalho4.X/Ports.c
+++ Trabalho4.X/Ports.c
@@ -7,14 +7,17 @@ void init_Ports(void){
 	/* Configure RA3 and RC1 (led 4 and led 5) as Output */
     TRISAbits.TRISA3 = 0;
     TRISCbits.TRISC1 = 0;
-    /* Configure pin AN0 as Input, for Read PT100 */
-    TRISAbits.TRISA0 = 1;
     
 	/*define initial values for RA3 and RC1*/
     PORTAbits.RA3 = 0;
     PORTCbits.RC1 = 0;
     
-    // Set RD0 as digital output - PWM
-    TRISDbits.TRISD0 = 0;
-    PORTDbits.RD0 = 1;
+    // Set RD1 as digital output - PWM - correspond OC2
+    TRISDbits.TRISD1 = 0;
+    PORTDbits.RD1 = 1;
+    
+    // Set RD2 as digital output - PWM - correspond OC3
+    TRISDbits.TRISD2 = 0;
+    PORTDbits.RD2 = 1;
+
 }
